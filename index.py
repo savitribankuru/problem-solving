ad5b47d79805bae04fc003ea669308009ae44a47
savitribankuru <savitribankuru23@gmail.com>
2025-05-19 11:31:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,28 +1,22 @@
-
 1.Print numbers from 1 to 10 using a loop.
 for i in range(1,11):
     print(i)
-
 2.Print all even numbers between 1 and 20.
 for i in range(0,21,2):
     print(i)
-
 3.Print the characters of the string "Python" one by one using a loop.
 s="python"
 for i in s:
     print(i)
-
 4.Print the sum of numbers from 1 to 100.
 s=0
 for i in range(1,101):
     s=s+i 
 print(s)
-
 5.Print multiplication table of 5 (like 5 x 1 = 5, ... 5 x 10 = 50).
 n=5
 for i in range(1,11):
     print(f"{n}*{i}={n*i}")
-
 6.Print all elements of a list using a loop.
 List=["apple", "banana", "cherry"]
 for i in List:
